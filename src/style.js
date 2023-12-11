f3414083e8a11abc7a80f774d5a7256bae4731f6
Suzanne Boersma <suzanneboersma@outlook.com>
2023-12-11 20:58:53 +0100

    style.js

---
+++ src/style.js
@@ -0,0 +1,79 @@
+const styles = {
+  
+  container: {
+    maxWidth: '900px',
+    margin: 'auto',
+    fontFamily: 'Arial, sans-serif',
+    color: 'F2E3DB',
+    borderRadius: '8px',
+    padding: 15,
+  },
+  heading: {
+    fontSize: 27,
+    marginBottom: '10px',
+    color: '263A29', 
+    fontFamily: 'Georgia, "Times New Roman", Times, serif',
+    
+  },
+  searchBox: {
+    width: '100%',
+    borderRadius: 'md',
+    backgroundColor: 'beige.100',
+    boxShadow: 'md',
+    marginBottom: '20px',
+  
+  },
+  image: {
+    borderRadius: '8px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    border: '2px solid #527853', 
+  },
+
+  text: {
+    marginBottom: '10px',
+    color: '263A29', 
+  },
+  boldText: {
+    fontWeight: 'bold',
+    color: '#38a169', 
+  },
+  button: {
+    backgroundColor: '#38a169',
+    color: '#fff',
+    padding: '10px 20px',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s ease',
+  },
+  link: {
+    color: 'orange',
+    textDecoration: 'none',
+    fontWeight: 'bold',
+   
+  },
+
+  underlinedLink: {
+    textDecoration: "underline",
+    color: '#EE7214'
+  },
+
+  recipeCard: {
+    
+    marginBottom: '20px',
+    
+  },
+  recipeTitle: {
+    color: '41644A',
+    fontSize: '1.2rem', 
+    fontWeight: 'bold',
+    fontFamily: 'Georgia, "Times New Roman", Times, serif', 
+  },
+  mealType: {
+    color: "#EE7214", // specific color for "Soort Gerecht:"
+  },
+
+
+};
+
+export default styles;
